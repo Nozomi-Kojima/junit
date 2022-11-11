e4823c6645b3cc8a4f5c251d63424a5bb779a335
Nozomi Kojima <nozomi.kojima@rakus-partners.co.jp>
2022-11-11 11:57:45 +0900

    enshu1完成

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -11,4 +11,16 @@ class StringUtilsTest {
 		String result = StringUtils.toSnakeCase("aaa");
 		assertEquals("aaa", result, "toSnakeCase(aaa)失敗");
 	}
+	
+	@Test
+	void toSnakeCase2() {
+		String result = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world", result, "toSnakeCase(hello_world)失敗");
+	}
+	
+	@Test
+	void toSnakeCase3() {
+		String result = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit", result, "toSnakeCase(practice_junit)失敗");
+	}
 }
---
+++ src/test/java/junit/tutorial/ex01/e03/CounterTest.java
@@ -0,0 +1,36 @@
+package junit.tutorial.ex01.e03;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import junit.tutorial.ex01.e02.Calculator;
+
+class CounterTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	@Test
+	void divide1() {
+		Calculator calc = new Calculator();
+			assertThrows(IllegalArgumentException.class, () -> calc.divide(2,0));
+	}
+
+}
