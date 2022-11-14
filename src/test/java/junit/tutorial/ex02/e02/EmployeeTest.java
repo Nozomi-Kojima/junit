20aedd597157c114df6ac39b491401d8ca38adad
Nozomi Kojima <nozomi.kojima@rakus-partners.co.jp>
2022-11-14 11:13:06 +0900

    11/13 enshu2 commit (完成)

--- src/test/java/junit/tutorial/ex02/e02/EmployeeTest.java
+++ src/test/java/junit/tutorial/ex02/e02/EmployeeTest.java
@@ -3,10 +3,8 @@ package junit.tutorial.ex02.e02;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -33,21 +31,16 @@ class EmployeeTest {
 	}
 
 	@Test
-	void employeeTest() throws Exception  {
-		InputStream sytemin = System.in;
-		// ファイルからストリームを作る
-		InputStream fileStream = new FileInputStream("/kyojima/springworkspace/junit/src/main/java/junit/tutorial/ex02/e02/Employee.txt");
-		// ストリームをバッファする
-		InputStream bufferedStream = new BufferedInputStream(fileStream); 
+	void employeeTest() {
+		InputStream input = getClass().getResourceAsStream("Employee.txt");
+		List<Employee> target = Employee.load(input);
+		Employee emp = target.get(0);
+		assertAll("employee", 
+				() -> assertEquals("Ichiro", emp.getFirstName(), "error:firstName"),
+				() -> assertEquals("Tanaka", emp.getLastName(), "error:lastName"),
+				() -> assertEquals("ichiro@example.com", emp.getEmail(), "error:eMail"));
+		}
 		
-		Employee target = new Employee();
-		target.load(bufferedStream);
-		assertAll("employee",
-			() -> assertEquals("Ichiro", target.getFirstName(), "error:firstName"),
-			() -> assertEquals("Tanaka", target.getLastName(), "error:lastName"),
-			() -> assertEquals("ichiro@example.com", target.getEmail(), "error:eMail")
-		);
-		bufferedStream.close();
 	}
 
-}
+
